969026dd7ac026a32f8e750fd6c1aa5d05fc68aa
joshuajrodrigues <rodrigues.joshr@gmail.com>
2024-05-03 22:24:37 +0530

    lottie transition fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import lottie from "astro-integration-lottie";
+import lottie from "astro-transitions-lottie";
 // https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
